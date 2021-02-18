995f3077e50ac2d778cb8b135b1660266b6f5d54
Anatoly <anatolijdegtyar@yandex.ru>
2021-02-18 15:17:16 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -27,11 +27,13 @@ let resize = document.querySelector(".example__resize");
 let mobileWidthOnly = window.matchMedia("(max-width: 767px)");
 let tabletWidth = window.matchMedia("(min-width: 768px)");
 
-if (mobileWidthOnly.matches) {
-  range.value = 1;
+function mobileWidth () {
+  range.value = 165;
+  imageAfter.classList.remove("example__image-visible");
+  imageBefore.classList.add("example__image-visible");
   range.onchange = function(evt) {
     evt.preventDefault();
-  if (range.value === "547") {
+  if (range.value >= "250") {
     imageBefore.classList.remove("example__image-visible");
     imageAfter.classList.add("example__image-visible");
     } else {
@@ -39,17 +41,51 @@ if (mobileWidthOnly.matches) {
       imageBefore.classList.add("example__image-visible");
     }
   };
-}
+};
 
-if (tabletWidth.matches) {
+function minTablet () {
   range.oninput = function () {
     resize.classList.remove("example__transition");
     resize.style.width = range.value + "px";
   }
-
   range.addEventListener("mouseleave", function (event) {
     range.value = 50 + "%";
     resize.style.width = 368 + "px";
     resize.classList.add("example__transition");
   });
+};
+
+let resizeTablet = function minTablet () {
+  range.oninput = function () {
+    resize.classList.remove("example__transition");
+    resize.style.width = 311 + "px";
+  }
+  range.addEventListener("mouseleave", function (event) {
+    if (imageBefore.classList.contains("example__image-visible")) {
+      range.value = 165;
+    } else {
+        range.value = 547;
+      }
+    resize.style.width = 311 + "px";
+    resize.classList.remove("example__transition");
+  });
+};
+
+if (window.innerWidth > 767) {
+  window.onload = minTablet();
+} else {
+  window.onload = mobileWidth();
 }
+
+window.addEventListener("resize", function() {
+  if (window.innerWidth > 767) {
+    minTablet();
+  }
+});
+
+window.addEventListener("resize", function() {
+  if (window.innerWidth < 768) {
+    mobileWidth();
+    resizeTablet();
+  }
+});
